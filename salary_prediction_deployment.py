1cfb7d9e10421ec2f5fcb400968a77400534a930
riddhikadam252-art <riddhikadam252@gmail.com>
2026-01-18 13:24:48 +0530

    Update salary_prediction_deployment.py

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -12,7 +12,7 @@ import pandas as pd
 import joblib
 
 model = joblib.load('model.pkl')
-encoders = joblib.load('label_encoder_sp.pkl')
+encoders = joblib.load('label_encoder.pkl')
 
 st.title('salary prediction app')
 
